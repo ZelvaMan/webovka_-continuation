63a9a120aa7e70764c93fa9ba46c6ab72b7e4c97
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-05-31 21:52:16 +0200

    jn

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php
 require "domu/domu_head.phtml";
-require "<everywhere/navigation.phtml";
+require "everywhere/everywhere_head.phtml";
+require "everywhere/navigation.phtml";
 require "domu/domu_main.phtml";
 require "everywhere/footer+end.phtml";
