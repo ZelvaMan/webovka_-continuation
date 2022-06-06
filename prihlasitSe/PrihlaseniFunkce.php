9e4f382418cfd70d32d2199c6363838b89379c7a
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-06-06 22:04:17 +0200

    sranda palece

--- prihlasitSe/PrihlaseniFunkce.php
+++ prihlasitSe/PrihlaseniFunkce.php
@@ -1,6 +1,16 @@
 <?php
-require "php_universal_functions/dbPripojeni.php";
-function Kontrola_Prihlaseni($db,$uzivatelInfo)
+function Kontrola_Prihlaseni($db, $uzivatelInfo)
 {
-   // $sql =" select * from users where name = {'$uzivatelInfo["P"]}"
+   if (strpos($_POST["p_EmailOrname"], "@")) {
+      query("email", $db, $uzivatelInfo);
+   } else {
+    query("name", $db, $uzivatelInfo);
+   }
+}
+function query($term, $db, $uzivatelInfo)
+{
+   $hashed_passwd = password_hash($uzivatelInfo["p_passwd"], PASSWORD_DEFAULT);
+   $dotaz = 'select * from users where '.$term."= '{$uzivatelInfo["p_EmailOrname"]}' and password=".$hashed_passwd;
+   mysqli_query($db, $dotaz);
+   json_encode(mysqli_query($db, $dotaz));
 }
