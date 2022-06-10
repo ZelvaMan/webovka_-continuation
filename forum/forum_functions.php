0daca581667d6f847de686bf9009bc30bd2defcd
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-06-10 20:53:43 +0200

    posts working

--- forum/forum_functions.php
+++ forum/forum_functions.php
@@ -1,9 +1,23 @@
 <?php
-function ulozPrispevek($db,$prispevekInfo)
+function savePost($db,$prispevekInfo)
 {
     $dotaz="insert into posts (title,Uid,content)
      values ('{$prispevekInfo["title"]}',
     '{$_SESSION['user']['Uid']}',
     '{$prispevekInfo["postContent"]}')";
       mysqli_query($db,$dotaz);
+}
+function getPosts($db)
+{
+    $dotaz= 'select * from posts';
+    return  $result=mysqli_query($db,$dotaz);
+   /* $qery_result=mysqli_fetch_assoc($result);
+    var_dump($qery_result);
+     $qery_result;*/
+}
+function getUser($db,$post){
+    $dotaz = 'select * from users where '.$post['Uid']." =  Uid";
+    $result= mysqli_query($db, $dotaz);
+    $qery_result=mysqli_fetch_assoc($result);
+    return $qery_result;
 }
