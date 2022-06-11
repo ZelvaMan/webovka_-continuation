6ec328c2296fcd202cda23682059dc1425a29b78
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-06-11 18:36:06 +0200

    commit

---
+++ half-life2.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+require "everywhere/everywhere_head.phtml";
+require "half-life/half-life_head.phtml";
+require "everywhere/navigation.phtml";
+require "half-life2/half-life2_main.phtml";
