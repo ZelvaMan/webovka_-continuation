023e983edebeff13338e6cd0cd39be375e5cae93
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-06-13 20:32:53 +0200

    code cleaning

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-
 <?php
 session_start();
-require "domu/domu_head.phtml";
+require "everywhere/everywhere_head.phtml";
 require "everywhere/everywhere_head.phtml";
 require "everywhere/navigation.phtml";
+require "universal/button.phtml";
 require "domu/domu_main.phtml";
 require "everywhere/footer+end.phtml";
