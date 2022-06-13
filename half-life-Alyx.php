dac9201aa576c20a7c92f3c842eda7546054eebd
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-06-13 19:20:20 +0200

    jn

---
+++ half-life-Alyx.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+require "everywhere/everywhere_head.phtml";
+require "half-life/half-life_head.phtml";
+require "everywhere/navigation.phtml";
+require "universal/button.phtml";
+require "half-lifeAlyx/hla.phtml";
+require "everywhere/footer+end.phtml";
---
+++ half-life-episode2.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+require "everywhere/everywhere_head.phtml";
+require "half-life/half-life_head.phtml";
+require "everywhere/navigation.phtml";
+require "universal/button.phtml";
+require "half-life2/hl2e2.phtml";
+require "everywhere/footer+end.phtml";
